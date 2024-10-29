b2e20a0616f228ee2257f4c1b1a7f02f66730b36
carlosnacamura <127620688+carlosnacamura@users.noreply.github.com>
2024-10-29 11:17:09 -0300

    Add files via upload

---
+++ Model/Entitidades/UsuariosEntidade.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Entitidades
+{
+    public class UsuariosEntidade
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+        public bool Ativo { get; set; }
+        public object[] Linha()
+        {
+            return new object[] { Id, Login, Senha, Ativo };
+        }
+    }
+}
